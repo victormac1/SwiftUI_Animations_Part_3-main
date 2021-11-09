3d096d2333604401b339532eee6d452ea8e6efa7
victormac1 <victorcognito@yahoo.com>
2021-11-09 13:30:06 +0100

    first commit

---
+++ Hue Rotation Example 1/Hue_Rotation_Example_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hue_Rotation_Example_1App.swift
+//  Hue Rotation Example 1
+//
+//  Created by Victors Code on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Hue_Rotation_Example_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
